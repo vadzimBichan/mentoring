894685263c250bfdbad4e05994ec13ea8dead493
Vadzim Bichan <vadzim.bichan@gmail.com>
2024-07-12 14:11:15 +0300

    [M5] Ignore generated *.feature.cs files

---
+++ Epam.ReportPortal.Automation/Epam.ReportPortal.Automation.UiTests/StepsDefinitions/EditDashboardsStepsDefinitions.cs
@@ -0,0 +1,30 @@
+﻿namespace Epam.ReportPortal.Automation.UiTests.StepsDefinitions;
+
+[Binding]
+public sealed class EditDashboardsStepsDefinitions
+{
+    private readonly ScenarioContext _scenarioContext;
+
+    public EditDashboardsStepsDefinitions(ScenarioContext scenarioContext)
+    {
+        _scenarioContext = scenarioContext;
+    }
+
+    [Given(@"\[context]")]
+    public void GivenContext()
+    {
+        _scenarioContext.Pending();
+    }
+
+    [When(@"\[action]")]
+    public void WhenAction()
+    {
+        _scenarioContext.Pending();
+    }
+
+    [Then(@"\[outcome]")]
+    public void ThenOutcome()
+    {
+        _scenarioContext.Pending();
+    }
+}
