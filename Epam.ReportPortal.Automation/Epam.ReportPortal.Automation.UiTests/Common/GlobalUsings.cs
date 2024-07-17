86f8aff3eea6b858a4b935ccfced27715a3f64c8
Vadzim Bichan <vadzim.bichan@gmail.com>
2024-07-17 09:26:51 +0300

    [M5] Add At least parallelization on fixture level for SpecFlow

--- Epam.ReportPortal.Automation/Epam.ReportPortal.Automation.UiTests/Common/GlobalUsings.cs
+++ Epam.ReportPortal.Automation/Epam.ReportPortal.Automation.UiTests/Common/GlobalUsings.cs
@@ -1,2 +1,4 @@
 global using NUnit.Framework;
 global using TechTalk.SpecFlow;
+
+[assembly: Parallelizable(ParallelScope.Fixtures)]  // https://docs.specflow.org/projects/specflow/en/latest/Execution/Parallel-Execution.html
--- Epam.ReportPortal.Automation/Epam.ReportPortal.Automation.UiTests/StepsDefinitions/Base/Hooks.cs
+++ Epam.ReportPortal.Automation/Epam.ReportPortal.Automation.UiTests/StepsDefinitions/Base/Hooks.cs
@@ -1,4 +1,6 @@
-﻿using Epam.ReportPortal.Automation.CoreSelenium.Base;
+﻿using Epam.ReportPortal.Automation.Configuration.Logger;
+using Epam.ReportPortal.Automation.Configuration.Settings;
+using Epam.ReportPortal.Automation.CoreSelenium.Base;
 
 namespace Epam.ReportPortal.Automation.UiTests.StepsDefinitions.Base;
 
@@ -6,14 +8,27 @@ namespace Epam.ReportPortal.Automation.UiTests.StepsDefinitions.Base;
 // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
 public sealed class Hooks
 {
+    [BeforeTestRun]
+    public void BeforeTestsRun()
+    {
+        ConfigurationManager.GetConfiguration();
+        LogConfiguration.Setup();
+    }
+
+    [AfterTestRun]
+    public void AfterTestsRun()
+    {
+        // not used at the moment
+    }
+
     [BeforeScenario]
-    public void BeforeScenario()
+    public void BeforeEachScenario()
     {
         Browser.GetInstance();
     }
 
     [AfterScenario]
-    public void AfterScenario()
+    public void AfterEachScenario()
     {
         Browser.Close();
     }
